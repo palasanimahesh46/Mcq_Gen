f9aae5f64e418c5f312048535ecf5faf403137aa
palasanimahesh46 <palasanimahesh46@gmail.com>
2024-01-17 10:00:57 +0530

    updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages,setup
 setup(
     name='mcqgenrator',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='Palasani Mahesh',
+    author_email='palasanimahesh46@gmail.com',
     install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
